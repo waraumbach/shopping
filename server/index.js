4555d221055aeb2abc6eb2b112b8f9eed5d60029
wara <144021938+waraumbach@users.noreply.github.com>
2024-06-17 13:27:11 +0200

    defining middleware

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const app = express()
 //middleware : access path go to parameter 
 //middleware : declaring middleware add app.get access path ,calling middleware and reload localhost 
 //on terminal shows middleware
-//const middleware =(req,res)=>console.log('This is a middleware')
+const middleware =(req,res,next)=>{console.log(`This is a middleware`)
+next()
+}
 
 app.get('/',(req,res)=>{
     res.send('welcome to my ecommerce api')
