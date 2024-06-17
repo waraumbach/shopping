c54696a6f755ea7cdde09e75ceb36ded1eba6547
wara <144021938+waraumbach@users.noreply.github.com>
2024-06-17 23:48:58 +0200

    dotenv,mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+
+const PORT =process.env.PORT ||3000
+const MONGO_URI =process.env.MONGO_URI
 
 const app = express()
+
+
 
 
 //middleware : access path go to parameter 
 //middleware : declaring middleware add app.get access path ,calling middleware and reload localhost 
 //on terminal shows middleware
-const middleware =(req,res,next)=>{console.log(`This is a middleware`)
+/*const middleware =(req,res,next)=>{console.log(`This is a middleware`)
 next()
-}
+}*/
 
 app.get('/',(req,res)=>{
     res.send('welcome to my ecommerce api')
@@ -16,4 +24,6 @@ app.get('/',(req,res)=>{
 })
 
 
-app.listen(3000, ()=> console.log(`Server is running on port 3000`))
+mongoose.connect(MONGO_URI)
+.then(()=> app.listen(PORT, ()=> console.log(`Server is running on  port ${PORT}`)))
+.catch(err=>console.log(`${err}😡`))
