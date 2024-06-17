f64e99b56135cacb83bc647a9b1aec03e059125a
wara <144021938+waraumbach@users.noreply.github.com>
2024-06-17 12:43:06 +0200

    localhost

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send('welcome to my ecommerce api')
+
+})
+
+
+app.listen(3000, ()=> console.log(`Server is running on port 3000`))
